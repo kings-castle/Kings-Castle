5fe27732bebff211a18ca65654f755032b6b048b
cooperrholmes@gmail.com <holmescr@acad.sunybroome.edu>
2019-08-18 23:56:03 +0000

    Added player model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_101659) do
+ActiveRecord::Schema.define(version: 2019_08_18_233617) do
+>>>>>>> Added player model and migration
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_08_18_101659) do
     t.integer "x_pos"
     t.integer "y_pos"
     t.integer "game_id"
+    t.integer "player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.integer "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
