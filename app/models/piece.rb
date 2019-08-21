947b5a5f3ff50e45cd72f8e2b9123af87db7d098
Matt Arrick <mattarrick@gmail.com>
2019-08-21 23:34:37 +0000

    Set up pieces STI

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,31 @@
 class Piece < ApplicationRecord
+  attr_accessor :color, :postion
+
   belongs_to :games
   belongs_to :players
+
+  self.inheritance_column = :piece_type
+
+  def self.piece_types
+    %w(Pawn Rook Knight Bishop Queen King)
+  end
+
+end
+
+class Pawn < Piece
+end
+
+class Rook < Piece
+end
+
+class Knight < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
 end
