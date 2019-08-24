b205ff450dd7b59866a7d786d87b609f54ac7255
Madison Cooper <madisondcooper@gmail.com>
2019-08-24 00:15:31 +0000

    added all pieces for STI

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,13 @@
 class Piece < ApplicationRecord
+  attr_accessor :color, :postion
+
   belongs_to :games
   belongs_to :players
+
+  self.inheritance_column = :piece_type
+
+  def self.piece_types
+    %w(Pawn Rook Knight Bishop Queen King)
+  end
+
 end
