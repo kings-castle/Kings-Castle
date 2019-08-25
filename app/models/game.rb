3d1d3c7ad4a2bb90d7a696d04ddddfb90d0c9ba1
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-25 23:31:27 +0000

    Initializing model method for games

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,5 +7,22 @@ class Game < ApplicationRecord
   def self.available_games(user_id)
   
   end
+
+
+  def available_games
+    #call all games
+    Games.each do |game|
+      #look up each game by the game_id
+      @game = Game.find_by_id(params[:id])
+      #test if black player id is nil and return results
+      if black_player_id == nil
+        puts "Game number #{game.game_id} is ready to play!"
+      else
+        puts "Game Unavailable"
+      end
+    end
+  end
+
+  
 end
 
