365355a67841ac1fdf61e1e9e5a24d80550ad813
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-26 18:39:28 +0000

    editing available_games

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,14 +5,14 @@ class Game < ApplicationRecord
 
 
   #sort available games for user to join
-  def self.available_games(user.id)
+  def self.available_games(user_id)
     #build an array to store available games
     available_games = []
     #iterate through all available games
     Games.each do |game|
       #pull any game that has a black_player_id of nil and
       #white_player_id does not equal the user
-      if black_player_id == nil && white_player_id =! user.id
+      if black_player_id == nil && white_player_id =! user_id
         #store pulled games in an array called available_games
         available_games << @game
         #return available_games
