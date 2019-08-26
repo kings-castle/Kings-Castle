b4148c780ab35a7abce463dc7f0bbed545d488ec
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-26 19:18:37 +0000

    Building method for available_games in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,9 +5,12 @@ class Game < ApplicationRecord
 
 
   #sort available games for user to join
+  #call using Game.available_games(user_id)
   def self.available_games(user_id)
+    #set user_id equal to the current user
+    user_id = current_user.id
     #iterate through all games
-    Games.each do |game|
+    Game.each do |game|
       #pull any game that has a black_player_id of nil and
       #white_player_id does not equal the user
       if black_player_id == nil && white_player_id =! user_id
