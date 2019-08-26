46dcc0af321cda80dbcb9ba0dbb62b6b4b95f1d1
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-26 20:28:03 +0000

    Testing relationship between game and users

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,19 +2,21 @@ class Game < ApplicationRecord
   has_many :players
   has_many :pieces
 
+  belongs_to :user
+
 
   #sort available games for user to join
   #call using Game.available_games(user_id)
   def self.available_games(user_id)
     #set user_id equal to the current user
-    #user_id = current_user.id
+    user_id = current_user.user_id
     #call all games
     @games = Game.all
     #iterate through all games
     @games.each do |game|
       #pull any game that has a black_player_id of nil and
       #white_player_id does not equal the user
-      if black_player_id == nil && white_player_id =! user_id
+      if black_player_id == nil && white_player_id != user_id
         #return the game_id of all available_games
         puts @game.id
       else
