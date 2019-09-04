621b2604430ef94f25442353256f5c07867de6f3
Matt Arrick <mattarrick@gmail.com>
2019-09-04 16:09:27 +0000

    Travis_CI Testing

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do 
-  describe "games#indxex action" do
+  describe "games#index action" do
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
