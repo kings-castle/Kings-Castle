f6f5c263a92be3b286e07882bc12676dfc607bff
Matt Arrick <mattarrick@gmail.com>
2019-09-06 23:57:19 +0000

    Added comments to changes made

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -12,23 +12,9 @@ RSpec.describe GamesController, type: :controller do
   describe "games#create action" do
     it "should successfully create a new game in our database" do
       w = User.create(email: 'mattarrick@gmail.com', password: 'password')
-      # game = Factorybot.create(:game)
       game = Game.create(name: 'User', white_player_id: 1, black_player_id: 1)
-      # game.save
-      # puts game
-      # post :create, params: {
-      #   game: { 
-      #     name: 'User', 
-      #     white_player_id: 1, 
-      #     black_player_id: 2 
-      #   }
-      # }
       g = Game.last
-      # games = Game.all
-      # game = games[0]
-      # puts games
-      # expect(game.name).to eq("User")
-      expect(g.white_player_id).to eq 1
+      expect(game.name).to eq("User")
     end
 
   end 
