fc76cdda25a4f41f5f3bc915c3c65fa398b8627c
Samantha Lee <samanthal.lee3@gmail.com>
2019-09-10 17:40:40 +0000

    Redefining variable for obstructing piece at end point

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -43,14 +43,16 @@ end
 
 #capture logic- test for color of obstructed piece and capture if opponent
 def self.capture(x,y)
+  #store the piece at the desired endpoint
+  end_point_piece = Piece.where({ x_pos: x, y_pos: y})
   #if the piece is obstructed and the colors are the same
-  if @piece.is_obstructed? == true && opponent_piece.color_name == @piece.color_name
+  if @piece.is_obstructed? == true && end_point_piece.color_name == @piece.color_name
     #return statement that the pieces are the same
     puts "Your own piece is already in that spot, silly"
   #if the piece is obstructed and the colors are different 
   else
     #reassign the new piece and opponent piece location
-    opponent_piece.update_attributes(x_pos: nil, y_pos: nil)
+    end_point_piece.update_attributes(x_pos: nil, y_pos: nil)
     @piece.update_attributes(x_pos: x, y_pos: y)
   end
 end
