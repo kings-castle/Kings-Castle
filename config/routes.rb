da5363f279f86ff1c398e2947a1cb0dd5ee7e1e3
Matt Arrick <mattarrick@gmail.com>
2019-09-11 18:21:42 +0000

    Revert "Merge branch 'support-moving-pieces-v2' of github.com:kings-castle/Kings-Castle"
    This reverts commit b79c4db9a9ed19191ab895666fd4d801bcd4ec38, reversing
    changes made to 0c06cf86876e60a0ca63225c873ef0836833f1c2.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'games#index'
   resource :games, only: [:show, :create]
-  resources :pieces
 end
