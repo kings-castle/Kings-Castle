70a4a26b4a3236928d7a3b55c9328ec2524058d7
Samantha Lee <samanthal.lee3@gmail.com>
2019-09-15 17:38:51 +0000

    Wrote and tested move and capture methods in the piece model to allow pieces to move to empty spaces and pieces to capture opponent pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -21,65 +21,61 @@ class Piece < ApplicationRecord
     color ? 'white' : 'black'
   end
 
+  
+
+### Added by Samantha Lee 9/15/2019
+
 #allows you to find a test a piece in a game
-#returns the piece at the given x,y location
+#returns array of the piece at the given x,y location
 def find_piece(x,y)
-  @piece = self.game.pieces.where(x_pos: x, y_pos: y)
-  puts @piece
-end
-
-def find_piece_player_id(x,y)
-   @piece = self.game.pieces.where(x_pos: x, y_pos: y)
-   puts @piece.player_id
+  piece = self.game.pieces.where(x_pos: x, y_pos: y)
+  puts piece
 end
 
 
-#test for empty endpoint and move player
+#test for empty endpoint and move player if the spot is empty
 def move(x,y)
   #returns true if piece exists
   #returns false if piece does not exist
   endpoint_piece = Piece.exists?(:x_pos => x, :y_pos => y)
-  #if the endpoint is empty
+  #if the endpoint is empty, or the piece does not exist
   if endpoint_piece == false
     #relocate the moving piece to the endpoint
     #update the x_pos and y_pos
     self.update_attributes(:x_pos => x, :y_pos => y)
+  #if the piece does exist, do nothing
   else
   end
 end
 
 
-#test for obstructed endpoint and move player
-#captures piece at the current endpoint
-#returns error if pieces are the same color
+#test for occupied endpoint
+#captures piece at the current endpoint if the player_ids are different
+#returns error if the player_ids are the same
 def capture(x,y)
   #locates piece at endpoint
-  endpoint_piece = self.game.pieces.where(x_pos: x, y_pos: y)
-
-  #if endpoint_piece returns true and the colors are the same
-  if endpoint_piece != nil && endpoint_piece.player_id == self.player_id
+  endpoint = self.game.pieces.find_by_x_pos_and_y_pos(x,y)
+  #tests accurate location and access to object
+  #endpoint.player_id
+  #if a piece exists at the endpoint and the player_ids are the same
+  if endpoint != nil && endpoint.player_id == self.player_id
+    #lets the user know their piece is already in that spot
     puts "Your own piece is already in that spot, silly"
-
-  #if the find_piece method returns true and the colors are different
-  else endpoint_piece != nil && endpoint_piece.player_id != self.player_id
-    #update the x and y pos of opponent_piece to equal 0
-    endpoint_piece.update_attributes(:x_pos => nil, :y_pos => nil)
-    #update the x and y pos of moving piece to equal endpoint
+  #if a piece exists at the endpoint and the player_ids are different
+  elsif endpoint != nil && endpoint.player_id != self.player_id
+    #update the x and y pos of endpoint piece to equal 0, remove piece from the board
+    endpoint.update_attributes(:x_pos => nil, :y_pos => nil)
+    #update the x and y pos of moving piece to equal endpoint, relocate the moving piece
     self.update_attributes(:x_pos => x, :y_pos => y)
+  else
   end
 end
 
-#right now my blocker is that I cannot access player_id as an attribute of a piece
-#it seems like when I quere for a piece, it does not store in the variable
-#so when I try to call the attribute on the variable, it does not show up
+### End addition by Samantha Lee 9/15/2019
 
 end
 
-#practice with ways to query db for piece
-    #Piece.where({ x_pos: x, y_pos: y})
-    #Piece.where("x_pos = ? AND y_pos = ?", x, y)
-    #Piece.where(x_pos: x, y_pos: y)
-    #Piece.exists?(:x_pos => x, :y_pos => y) 
+
 
 
 
