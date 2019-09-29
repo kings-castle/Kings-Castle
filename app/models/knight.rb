0292f4fc355120468717aeb60cce2ae52238a2e3
Samantha Lee <samanthal.lee3@gmail.com>
2019-09-29 16:15:14 +0000

    Building valid move for night and calling valid_move? == true in capture method

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,35 @@
 class Knight < Piece
+
+  ### Created by Samantha Lee 9/29/2019
+
+  #Knight can move to the following endpoints
+    #x+1, y+2
+    #x+1, y-2
+    #x+2, y+1
+    #x+2, y-1
+    #x-1, y+2
+    #x-1, y-2
+    #x-2, y-1
+    #x-2, y+1
+
+    #check to see if the passed endpoint is equal to any of the possible endpoint locations
+
+  def valid_move?(x, y)
+    if  
+      x = self.x_pos + 1 && y = self.y_pos + 2 ||
+      x = self.x_pos + 1 && y = self.y_pos - 2 ||
+      x = self.x_pos + 2 && y = self.y_pos + 1 ||
+      x = self.x_pos + 2 && y = self.y_pos - 1 ||
+      x = self.x_pos - 1 && y = self.y_pos + 2 ||
+      x = self.x_pos - 1 && y = self.y_pos - 2 ||
+      x = self.x_pos - 2 && y = self.y_pos - 1 ||
+      x = self.x_pos - 2 && y = self.y_pos + 1
+
+        true
+      else
+        false
+    end
+  end
+
+
 end
